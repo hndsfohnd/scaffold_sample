ae0131840d2b523b4bdb6c36993574f07a813f72
hndsfohnd <g532848_nj@yahoo.co.jp>
2020-04-17 11:51:01 +0900

    Update 20200410112342_remove_columns_from_books.rb

--- db/migrate/20200410112342_remove_columns_from_books.rb
+++ db/migrate/20200410112342_remove_columns_from_books.rb
@@ -1,5 +1,7 @@
 class RemoveColumnsFromBooks < ActiveRecord::Migration[5.2]
   def change
-    remove_column :Books, :author_id, :bigint
+    remove_foreign_key :books, :authors
+    remove_index :books, :author_id
+    remove_reference :books, :author
   end
 end
